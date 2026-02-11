c2bf90b9bb97305725636e2a3c292f0970c88ece
Vineeth Shetty <mrvineethshetty@gmail.com>
2026-02-11 20:25:48 +0530

    Added smooth scroll reveal animation

---
+++ script.js
@@ -0,0 +1,16 @@
+function revealOnScroll() {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((element) => {
+    const windowHeight = window.innerHeight;
+    const elementTop = element.getBoundingClientRect().top;
+    const revealPoint = 100;
+
+    if (elementTop < windowHeight - revealPoint) {
+      element.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("load", revealOnScroll);
